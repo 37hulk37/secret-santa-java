ca6d129033ff4d63e469a7e4bd9cc9a1fb45beba
37hulk37 <Alex.03spb@mail.ru>
2023-01-23 20:53:32 +0300

    test client

--- test.py
+++ test.py
@@ -1,20 +1,42 @@
 import requests
 
-r = requests.post('http://127.0.0.1/register-user', json={"name": "Jason Burne"})
+r = requests.post('http://127.0.0.1/register-user', json={
+    "name": "Jason Burne"})
 print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
 
 r = requests.get('http://127.0.0.1/get-users')
 print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
 
 r = requests.post('http://127.0.0.1/create-group', json={
-  "id": "1",
-          "groupName": "agents"})
+   "userId": "1",
+        "groupName": "agents"})
 print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
 
 r = requests.get('http://127.0.0.1/get-groups')
 print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
 
-r = requests.put('http://127.0.0.1/close-group', json={
-  "username": "Jason Bourne",
-          "groupName": "0"})
+r = requests.post('http://127.0.0.1/register-user', json={
+    "name": "Roger Waters"})
+print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+r = requests.post('http://127.0.0.1/add-to-group', json={
+    "userId": "2",
+        "groupName": "agents"})
+print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+##r = requests.post('http://127.0.0.1/leave-group', json={
+##   "userId": "2",
+##           "groupName": "agents"})
+##print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+r = requests.get('http://127.0.0.1/get-groups')
+print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+r = requests.post('http://127.0.0.1/start-secret-santa', json={
+   "userId": "1",
+           "groupName": "agents"})
+print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+r = requests.get('http://127.0.0.1/get-recipient', json={
+    "userId":"1"})
 print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
