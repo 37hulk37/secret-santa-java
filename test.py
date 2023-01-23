48826248c01fe840f9fb8bb0ad6b4fdfae7581f7
37hulk37 <Alex.03spb@mail.ru>
2023-01-23 16:12:28 +0300

    start-sacret-santa

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import requests
 
-r = requests.post('http://127.0.0.1/register-user', data={"name": "Jason Burne"})
+r = requests.post('http://127.0.0.1/register-user', json={"name": "Jason Burne"})
 print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
 
-r = requests.post('http://127.0.0.1/create-group', data={
-  "name": "Jason Bourne",
-          "group_name": "Agents"})
+r = requests.get('http://127.0.0.1/get-users')
+print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+r = requests.post('http://127.0.0.1/create-group', json={
+  "id": "1",
+          "groupName": "agents"})
+print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+r = requests.get('http://127.0.0.1/get-groups')
+print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
+
+r = requests.put('http://127.0.0.1/close-group', json={
+  "username": "Jason Bourne",
+          "groupName": "0"})
 print(f"Status Code: {r.status_code}, Response: {r}, {r.content}")
